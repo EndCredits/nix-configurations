72452c333cc2999a31f1ca554c80a37333f1a96f
EndCredits <alicization.han@gmail.com>
2021-09-10 15:01:46 +0000

    configs:gpu: Enable modesetting for Nvidia Drivers
    This fixed error:
    Failed assertions:
    - You cannot use wayland with GDM without modesetting enabled for NVIDIA drivers, set `hardware.nvidia.modesetting.enable = true`

--- configs/gpu-acceleration.nix
+++ configs/gpu-acceleration.nix
@@ -3,6 +3,7 @@
 {
   # Enable Nvidia Graphic Drivers
   services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.modesetting.enable = true;
 
   # Enable GDM Wayland session for Nvidia GPU
   services.xserver.displayManager.gdm.nvidiaWayland = true;
