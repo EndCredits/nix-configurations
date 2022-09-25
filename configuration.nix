960a5bc4776f2d1b4076c951e6594fcedd3ab9aa
EndCredits <alicization.han@gmail.com>
2022-09-25 19:05:34 +0800

    extra: Allow using experimental nix command
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- configuration.nix
+++ configuration.nix
@@ -57,6 +57,10 @@
    nix.gc.automatic = true;
    nix.gc.dates = "05:15";
 
+   nix.extraOptions = ''
+      experimental-features = nix-command
+   '';
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
