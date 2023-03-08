cc6dcc797452ab0ff6f247e9604d55a3953a7e6e
EndCredits <alicization.han@gmail.com>
2023-03-08 22:55:13 +0800

    configs: Add more extensions for vscode
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- configs/vscode.nix
+++ configs/vscode.nix
@@ -5,13 +5,16 @@
     (vscode-with-extensions.override {
       vscodeExtensions = with vscode-extensions; [
         ms-python.python
-	      vscodevim.vim
+	vscodevim.vim
       	yzhang.markdown-all-in-one
       	ms-vscode.cpptools
       	golang.go
       	pkief.material-icon-theme
       	zhuangtongfa.material-theme
         ms-azuretools.vscode-docker
+	vscode-extensions.bbenoist.nix
+	vscode-extensions.arrterian.nix-env-selector
+	vscode-extensions.jnoortheen.nix-ide
       ];
     })
   ];
