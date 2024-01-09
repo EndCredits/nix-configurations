b26473f57374b30b638f8417c1dd413b5cb7049d
EndCredits <endcredits@crepuscular-aosp.icu>
2024-01-09 20:56:57 +0800

    feat: Add back official nixos cache service public key
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- flake.nix
+++ flake.nix
@@ -13,6 +13,8 @@
       "https://nix-community.cachix.org"
     ];
     trusted-public-keys = [
+      # public key for nixos official cache server
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       # nix-community cache server public key
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
