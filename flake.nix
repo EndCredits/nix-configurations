ce8c12c1717c56e92c605cac7229221faa3331ae
EndCredits <endcredits@crepuscular-aosp.icu>
2024-01-09 19:10:48 +0800

    feat: Adapt flake system name to our hostname
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- flake.nix
+++ flake.nix
@@ -47,7 +47,7 @@
       # Run the following command in the flake's directory to
       # deploy this configuration on any NixOS system:
       #   sudo nixos-rebuild switch --flake .#nixos-main
-      "nixos-main" = nixpkgs.lib.nixosSystem {
+      "crepuscular-nixos" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
 
         # The Nix module system can modularize configuration,
