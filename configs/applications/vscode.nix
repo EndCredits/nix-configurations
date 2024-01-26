8a6e1d977932630793827bf9a3d0e19cfdb79743
EndCredits <endcredits@crepuscular-aosp.icu>
2024-01-26 16:49:30 +0800

    feat: drop vim vscode extension
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- configs/applications/vscode.nix
+++ configs/applications/vscode.nix
@@ -4,16 +4,15 @@
   environment.systemPackages = with pkgs; [
     (vscode-with-extensions.override {
       vscodeExtensions = with vscode-extensions; [
-	vscodevim.vim
-      	yzhang.markdown-all-in-one
+        yzhang.markdown-all-in-one
       	ms-vscode.cpptools
       	golang.go
       	pkief.material-icon-theme
       	zhuangtongfa.material-theme
         ms-azuretools.vscode-docker
-	vscode-extensions.bbenoist.nix
-	vscode-extensions.arrterian.nix-env-selector
-	vscode-extensions.jnoortheen.nix-ide
+        vscode-extensions.bbenoist.nix
+        vscode-extensions.arrterian.nix-env-selector
+        vscode-extensions.jnoortheen.nix-ide
       ];
     })
   ];
