e5c19ad70e032ca1c45932b52cf0031c80a71a2c
EndCredits <endcredits@crepuscular-aosp.icu>
2024-02-07 12:50:17 +0800

    feat: make libGL visible to obsidian
     * According to https://github.com/NixOS/nixpkgs/issues/282749 ,
       autoPatchelfHook won't add shared libraries to executables. Apps like
       electron using this way to lookup real library with a dummy library.
    
     * This is only a workaround for another workaround. Very dirty and not
       recommanded to use. This should be reverted after the issue closed
    
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- overlays/obsidian/default.nix
+++ overlays/obsidian/default.nix
@@ -2,8 +2,9 @@
 
 (self: super: {
   obsidian = super.obsidian.overrideAttrs (oldAttrs: {
-    buildInputs = oldAttrs.buildInputs or [] ++ [ super.makeWrapper ];
+    buildInputs = oldAttrs.buildInputs or [] ++ [ super.makeWrapper super.libGL ];
     postInstall = oldAttrs.postInstall or "" + ''
+      sed -i "1 aexport\ LD\_LIBRARY\_PATH\=${lib.makeLibraryPath [ super.libGL ]}\/\:\$LD\_LIBRARY\_PATH" $out/bin/obsidian
       sed -i "s/\"\$\@\"/\"\-\-ozone\-platform\=wayland\"\ \"\-\-ozone\-platform\-hint\=auto\"\ \"\-\-enable\-wayland\-ime\"\ \"\$\@\"/g" $out/bin/obsidian
       '';
   });
